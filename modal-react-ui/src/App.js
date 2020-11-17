a349fcadca3d6867bf69b7e021443445c62e7c55
devMarilia <devMarilia.frontend@gmail.com>
2020-11-17 19:20:41 -0300

    Modificaçao realizada

--- modal-react-ui/src/App.js
+++ modal-react-ui/src/App.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import styled from "styled-components"; 
-
-
-function App() {
-  return (
-    <>
-      <Container>
-        <Button>Whattt up</Button>  
-      </Container>
-    </>
-  );
-}
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import  { Modal }  from '../src/components/Modal';
+import { GlobalStyle } from './globalStyle';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh
+  height: 100vh;
 `;
 
 const Button = styled.button`
-  min-wdth: 100px;
+  min-width: 100px;
   padding: 16px 32px;
   border-radius: 4px;
   border: none;
   background: #141414;
+  color: #fff;
   font-size: 24px;
-  cursor: poiner;
+  cursor: pointer;
 `;
 
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(prev => !prev);
+  };
+
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </Container>
+    </>
+  );
+}
 
 export default App;
-
-
---
+++ modal-react-ui/src/globalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: boder-box;
+        margin: 0;
+        padding: 0;
+        font-family: 'Arial', sans-serife;
+    }
+`
