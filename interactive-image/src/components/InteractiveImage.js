a5e2a548cfb21cdfbafd84a8913bd683c18a18fd
devMarilia <devMarilia.frontend@gmail.com>
2020-11-20 19:43:19 -0300

    Finalizado projeto 3d

---
+++ interactive-image/src/components/InteractiveImage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './InteractiveImage.css'
+
+export default function(){
+    return(
+        <div className="container">
+            <div className="box">
+                <div className="face face1">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="face face2">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
